4dfbbbd4d206e00b736a2fed0847b9da15952f01
George Perry <george.perry@auckland.ac.nz>
2024-11-03 20:12:21 +1300

    fix: scenario visualisation (R)

--- src/r/visualisation/calibrate.r
+++ src/r/visualisation/calibrate.r
@@ -1,46 +1,54 @@
-
-# Load and deal with the baseline  runs for the bc hysteresis model
-# These address dynamics with no change but variable number of agents, units, and different learnings
+# Load and deal with the baseline runs for the bc hysteresis model
+# These address dynamics with different initial a - for calibration
+# models (GAMs)
 
 library(tidyverse)
 library(data.table)
+
 library(janitor)
 
 source("src/r/processSimsFuncs.r")
 
-process_bch_sims(base_path = "ms/data/calibrate",
+process_bch_sims(
+    base_path = "ms/data/calibrate",
     nl_file_name = "hysteresis calibrate-table",
-    save_file = "calibrate")
+    save_file = "calibrate"
+)
 
 save.image("ms/data/calibrate/calibrate.RData")
 
-tidy_bch_sims(base_path = "ms/data/calibrate",
-    zip_file = "calibrate")
+tidy_bch_sims(
+    base_path = "ms/data/calibrate",
+    zip_file = "calibrate"
+)
 
-save.image("output/data/calibrate/calibrate.RData")
+save.image("ms/data/calibrate/calibrate.RData")
 ####
 
 # basically want to plot mean k-a at 50 gens vs n-p-a by scenario
 load("ms/data/calibrate/calibrate.RData")
 
 # scenario labeller
-sc_labels <- c("a" = "null",
+sc_labels <- c(
+    "a" = "null",
     "b" = "spatial",
     "c" = "social",
     "d" = "spatial + social",
     "e" = "pref move",
     "f" = "spatial + pref move",
     "g" = "social + pref move",
-    "h" = "spatial + social + pref-move")
+    "h" = "spatial + social + pref-move"
+)
 
-calibrate_sc35 <- calibrate[gen == 35, 
-        .(mean_ka = mean(ka), range_ka = mean(ka.range)), 
-        by = .(scenario, sc_tag, rep, unit, n_p_a)]
+calibrate_sc35 <- calibrate[gen == 35,
+    .(mean_ka = mean(ka), range_ka = mean(ka.range)),
+    by = .(scenario, sc_tag, rep, unit, n_p_a)
+]
 
 c35_gg <- ggplot(data = calibrate_sc35) +
     geom_point(aes(x = n_p_a, y = mean_ka, col = factor(unit))) +
     scale_colour_brewer(type = "qual", palette = "Dark2") +
-   # geom_smooth(aes(x = n_p_a, y = mean_ka)) +
+    # geom_smooth(aes(x = n_p_a, y = mean_ka)) +
     facet_wrap(~sc_tag, labeller = labeller(sc_tag = sc_labels))
 
 c35_range_gg <- ggplot(data = calibrate_sc35) +
@@ -58,25 +66,31 @@ library(mgcv)
 calibrate_gam <- calibrate[gen == 35, c("rep", "ka", "n_p_a", "unit", "scenario")][, .(mean(ka)), by = .(rep, scenario, n_p_a)]
 
 # create a GAM for each scenario
+df <- data.frame(n_p_a = seq(0.05, 0.75, 0.025))
+
 gam_list <- vector(mode = "list", length = 8)
-pred_list <-  vector(mode = "list", length = 8)
+pred_list <- vector(mode = "list", length = 8)
 
 # loop over the scenarios
-for (s in 1:8)
-{
+for (s in 1:8) {
     x <- calibrate_gam[scenario == s]
     gam_list[[s]] <- gam(V1 ~ s(n_p_a), data = x)
-    pred_list[[s]] <- predict(gam_list[[s]], newdata = df, se.fit = TRUE) |> 
+    pred_list[[s]] <- predict(gam_list[[s]], newdata = df, se.fit = TRUE) |>
         data.frame() |>
-        mutate(n = seq(0.05, 0.75, 0.05))
+        mutate(n = seq(0.05, 0.75, 0.025))
 }
 
 # bind together
 pred_npa_gams <- bind_rows(pred_list, .id = "scenario")
+base50 <- filter(pred_npa_gams, n == 0.5 & scenario == 1)$fit
 
 # plot...
-# ggplot() +
-#     geom_point(data = calibrate_gam, aes(x = n_p_a, y = V1), col = "dark grey") +
-#     geom_line(data = pred_df, aes(x = n, y = fit), col = "red") +
-#     facet_wrap(~scenario) +
-#     theme_bw()
+calibrate_gams_gg <- ggplot() +
+    geom_point(data = calibrate_gam, aes(x = n_p_a, y = V1), col = "dark grey") +
+    geom_line(data = pred_npa_gams, aes(x = n, y = fit), col = "red") +
+    geom_hline(yintercept = base50, linetype = 2) +
+    geom_vline(xintercept = 0.5, linetype = 2) +
+    facet_wrap(~scenario) +
+    theme_bw()
+
+save.image("ms/data/calibrate/calibrate.RData")
--- src/r/visualisation/lossAmount.r
+++ src/r/visualisation/lossAmount.r
@@ -3,41 +3,73 @@ library(tidyverse)
 library(data.table)
 library(janitor)
 
-source("src/r/processSimsFuncs.r")
+# source("src/r/processSimsFuncs.r")
 
-process_bch_sims(base_path = "ms/data/amt-loss-return",
-    nl_file_name = "hysteresis amount-of-loss-return-table",
-    save_file = "amtLossReturn")
+# process_bch_sims(base_path = "ms/data/amt-loss-return",
+#     nl_file_name = "hysteresis amount-of-loss-return-table",
+#     save_file = "amtLossReturn")
 
-amt_loss_return <- sim_all
-gdata::keep(amt_loss_return, sure = TRUE)
-save.image("ms/data/amt-loss-return/amtLossReturn.RData")
+# amt_loss_return <- sim_all
+# gdata::keep(amt_loss_return, sure = TRUE)
+# save.image("ms/data/amt-loss-return/amtLossReturn.RData")
 
 
-source("src/r/processSimsFuncs.r")
+# source("src/r/processSimsFuncs.r")
 
-tidy_bch_sims(base_path = "ms/data/amt-loss-return",
-    zip_file = "amtLossReturn")
+# tidy_bch_sims(base_path = "ms/data/amt-loss-return",
+#     zip_file = "amtLossReturn")
 
+
+# ####
+# library(tidyverse)
+# library(data.table)
+# library(janitor)
+
+# source("src/r/processSimsFuncs.r")
+
+# process_bch_sims(base_path = "ms/data/rate-loss-return",
+#     nl_file_name = "hysteresis rate-of-loss-return-table",
+#     save_file = "rateLossReturn")
+
+# rate_loss_return <- sim_all
+# gdata::keep(rate_loss_return, sure = TRUE)
+# save.image("ms/data/amt-loss-return/rateLossReturn.RData")
+
+# source("src/r/processSimsFuncs.r")
+
+# tidy_bch_sims(base_path = "ms/data/rate-loss-return",
+#     zip_file = "rateLossReturn")
 
 ####
-library(tidyverse)
-library(data.table)
-library(janitor)
+# [["rate-return" 0 1]["rate-loss" 1]]
+# [["rate-return" 0 2]["rate-loss" 2]]
+# [["rate-return" 0 3]["rate-loss" 3]]
+# [["rate-return" 0 4]["rate-loss" 4]]
+# [["rate-return" 0 5]["rate-loss" 5]]
 
-source("src/r/processSimsFuncs.r")
+load("ms/data/amt-loss-return/amtLossReturn.RData")
 
-process_bch_sims(base_path = "ms/data/rate-loss-return",
-    nl_file_name = "hysteresis rate-of-loss-return-table",
-    save_file = "rateLossReturn")
+amt_loss_return[, no_return := rate_return == 0]
 
-rate_loss_return <- sim_all
-gdata::keep(rate_loss_return, sure = TRUE)
-save.image("ms/data/amt-loss-return/rateLossReturn.RData")
+sc <- 5
+x <- amt_loss_return[scenario == sc]|>
+    group_by(gen, rate_loss, rate_return, no_return, rep) |>
+    summarise(mka = mean(ka), fa = mean(fraction.a)) |>
+    ungroup()
 
-source("src/r/processSimsFuncs.r")
+df <- data.frame(n_p_a = x$fa)
+x$pp <- predict(object = s, newdata = df, type = "response")
 
-tidy_bch_sims(base_path = "ms/data/rate-loss-return",
-    zip_file = "rateLossReturn")
+y <- x |>
+    group_by(gen, rate_loss, rate_return, no_return) |>
+    summarise(fa = mean(fa), pp = mean(pp)) |>
+    ungroup()
 
-####
+ggplot() +
+    geom_line(data = x, aes(x = gen, y = mka, col = as.factor(rate_return), group = rep),alpha = 0.5) +
+    geom_line(data = y, aes(x = gen, y = fa * 100)) +
+    geom_line(data = y, aes(x = gen, y = pp), linetype = 2) +
+    facet_grid(no_return ~ rate_loss) +
+    scale_colour_brewer(type = "qual", palette = "Dark2", name = "Return rate") +
+    xlim(0,55) +
+    theme_bw()
